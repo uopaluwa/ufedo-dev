6a6aa3ef9eab5a67d1046d9a28d8137b3813e8b0
uopaluwa <ufedojo.opaluwa@andela.com>
2019-11-12 15:04:55 +0100

    extend tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        gold: '#ffd700'
+      },
+      boxShadow: {
+        logo: '4px 4px 2px #6b46c1'
+      }
+    }
   },
   variants: {},
   plugins: []
