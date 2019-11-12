fb76976c18738eb95cceec8df6e08a9d00ce5017
uopaluwa <ufedojo.opaluwa@andela.com>
2019-11-12 15:05:47 +0100

    add site icon

---
+++ src/components/logo.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Logo = () => {
+  return (
+    <div id="logo-three" className="text-center font-medium">
+      <div className="w-24 border-l-2 border-purple-700 bg-gold shadow-logo">Ufedojo</div>
+      <div className="w-24 border-l-2 border-purple-700 bg-gold ml-3 -mt-1 shadow-logo">Opaluwa</div>
+    </div>
+  )
+}
+
+export default Logo;
