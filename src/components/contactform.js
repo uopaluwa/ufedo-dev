ec03a4bf5bf5fe450f06299e4d3bea75c02693d2
uopaluwa <ufedojo.opaluwa@andela.com>
2019-11-19 17:17:31 +0100

    make contact from into a component

---
+++ src/components/contactform.js
@@ -0,0 +1,36 @@
+import React from "react"
+
+const ContactForm = () => (
+  <form className="mx-auto" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+    <div className="w-9/12 mx-auto">
+      <h1 className="text-3xl mb-2 mt-2 text-center">SAY HELLO</h1>
+      <p className="invisible">
+        <label>Don't fill this out if you're human: 
+        <input name="form-name" value="contact" /></label>
+      </p>
+      <div className="mb-4">
+        <label>
+          <span className="block">Name</span> 
+          <input className="w-full bg-gray-200 appearance-none border-2 border-gray-200 rounded py-1 px-1" type="text" name="name" />
+        </label>
+      </div>
+      <div className="mb-4">
+        <label>
+          <span className="block">Email</span> 
+          <input className="w-full bg-gray-200 appearance-none border-2 border-gray-200 rounded py-1 px-1" type="email" name="email" />
+        </label>
+      </div>
+      <div className="mb-4">
+        <label>
+          <span className="block">Message</span> 
+          <textarea rows="5" className="w-full bg-gray-200 appearance-none border-2 border-gray-200 rounded py-1 px-1" name="message"></textarea>
+        </label>
+      </div>
+      <div className="mt-4">
+        <button className="w-full font-semibold py-2 px-4 bg-purple-700 text-white rounded shadow mb-4" type="submit">Send</button>
+      </div>
+    </div>
+  </form>
+)
+
+export default ContactForm;
