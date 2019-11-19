3e3f067ab9330c3e299d245d12fea143da24bc03
uopaluwa <ufedojo.opaluwa@andela.com>
2019-11-19 18:07:53 +0100

    add coming soon notice to blog route

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const BlogPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <div className="sm:text-2xl md:text-4xl">🚧 Blog will be launched soon!! 🚧</div>
+  </Layout>
+)
+
+export default BlogPage
